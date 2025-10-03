f3c33cac3a783d72203ee9f5f4f0d67475ce0a9e
Kapyu <astraponystargazer@gmail.com>
2025-10-03 15:58:03 -0400

    idkk

--- websites/P/Pony Town/presence.ts
+++ websites/P/Pony Town/presence.ts
@@ -1,18 +1,20 @@
 import { Assets } from 'premid'
 
 const presence = new Presence({
-  clientId: '',
+  clientId: '1423759487852089395',
 })
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets { // Other default assets can be found at index.d.ts
-  Logo = '',
+  Logo = 'https://kapyu.neocities.org/otherassets/ponytownimagelarge.jpg',
 }
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
     largeImageKey: "https://kapyu.neocities.org/otherassets/ponytownimagelarge.jpg",
     startTimestamp: browsingTimestamp,
+    state: 'Pony Town',
+    details: 'Playing Pony Town',
     smallImageKey: Assets.Play,
   }
 
