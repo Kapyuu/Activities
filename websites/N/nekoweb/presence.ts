715a5085d8995e39d415711baabe4274605cf8fa
Kapyu <astraponystargazer@gmail.com>
2025-10-05 21:41:44 -0400

    nekoweb cooly

--- websites/N/nekoweb/presence.ts
+++ websites/N/nekoweb/presence.ts
@@ -1,19 +1,65 @@
-import { Assets } from 'premid'
+import { ActivityType, Assets } from 'premid'
 
-const presence = new Presence({
-  clientId: '',
-})
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
+const presence = new Presence({
+  clientId: '1424514702205124700',
+})
+
 enum ActivityAssets { // Other default assets can be found at index.d.ts
-  Logo = '',
+  Logo = 'https://kapyu.neocities.org/otherassets/nekowebicon.png',
 }
 
 presence.on('UpdateData', async () => {
+
+  const showSiteButton = await presence.getSetting<boolean>('showsite')
+
+  const { pathname } = document.location
+
   const presenceData: PresenceData = {
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
-    smallImageKey: Assets.Play,
+  }
+
+  if (pathname === '/') {
+    presenceData.details = 'Home'
+  } else if (pathname.includes('/explore')) {
+    presenceData.details = 'Browsing Sites'
+  }  else if (pathname.includes('/feed')) {
+    presenceData.details = 'Viewing Feed'
+  }  else if (pathname.includes('/news')) {
+    presenceData.details = 'Viewing News'
+  }  else if (pathname.includes('/terminal')) {
+    presenceData.details = 'Using the terminal'
+  }  else if (pathname.includes('/dashboard/editor')) {
+    presenceData.details = 'Editing a file'
+  }  else if (pathname.includes('/dashboard')) {
+    presenceData.details = 'Viewing Dashboard'
+  } else {
+    delete presenceData.details
+  }
+
+  if (showSiteButton === true) {
+    if (document.getElementById('userdisplay')) {
+      const userPage = document.getElementById('userdisplay')?.parentElement?.getAttribute('href')
+      const pageLink = 'https:' + String(userPage)
+      presenceData.buttons = [
+        {
+          label: 'Visit Site',
+          url: pageLink
+        }
+      ]
+      presenceData.smallImageKey = 'https://nekoweb.org' + String(document.getElementById('usersc')?.getAttribute('src'))
+      presenceData.smallImageUrl = pageLink
+    } else {
+      delete presenceData.buttons
+      delete presenceData.smallImageKey
+      delete presenceData.smallImageUrl
+    }
+  } else {
+    delete presenceData.buttons
+    delete presenceData.smallImageKey
+    delete presenceData.smallImageUrl
   }
 
   presence.setActivity(presenceData)
